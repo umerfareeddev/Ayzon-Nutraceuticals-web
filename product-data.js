a3923e7922699b530086b51429863f55c0983849
Umer-fareed21 <madfareed16@gmail.com>
2026-07-13 13:56:03 +0500

    push1

--- product-data.js
+++ product-data.js
@@ -1,80 +1,101 @@
 // Global Products Database Array
 const AYZONE_PRODUCTS = [
     {
-        id: "abiwin",
-        title: "Abiwin",
+        id: "tefit",
+        title: "TEFIT",
         category: "syrup",
-        img: "./images/syrup-imgs/Abiwin-Syrup.png",
+        img: "./images/syrup-imgs/TEFIT-Syrup.png",
         gallery: [
-            "./images/syrup-imgs/Abiwin-Syrup.png",
-            "./images/syrup-imgs/Abiwin-2.png",
-            "./images/syrup-imgs/Abiwin-3.png"
+            "./images/syrup-imgs/TEFIT-Syrup.png",
+            "",
+            ""
         ],
-        shortDescription: "Supports immunity and reduces inflammation.",
+        shortDescription: "Maximum Protein Provider with Lysine & Iron. Protein supplement for all ages in all seasons.",
         details: {
-            Composition: "Serratia Marcescens 10mg",
-            Dosage: "1 teaspoon twice daily.",
-            Indication: "Used for swelling and inflammation.",
-            Storage: "Store below 30°C.",
-            Warning: "Keep out of children's reach."
+            Indications: `TEFIT helps in rejuvenating the whole body system and provides energy required for extra stress.
+            Infants and children growth.
+            Alleviates chronic pain.
+            Enhances pregnant and lactating women vitality.
+            Stabilizes energy swings.
+            Speeds up recovery from genital herpes and prevent recurrences.
+            Rebuilds immune-fighting cells.
+            Helps control chronic renal (kidney) failure.
+            Relieves arthritis.
+            Cases of oligospermia (Subfertility).
+            Restores brain function balance.`,
+
+            ContraIndications: "Consult your doctor if you are diabetic patient.",
+
+            Composition: `Each 5ml contains:
+                            Soluble Fish Protein 250mg
+                            Alanine 17.4mg
+                            Arginine 10.25mg
+                            Aspartic Acid 15.75mg
+                            Cystine 2.1mg
+                            Glutamic Acid 18.5mg
+                            Glycine 12.4mg
+                            Histidine 5mg
+                            Isoleucine 14.1mg
+                            Leucine 19.55mg
+                            Lysine 21.25mg
+                            Methionine 7.45mg
+                            Phenylalanine 10.2mg
+                            Proline 9.55mg
+                            Serine 8.7mg
+                            Threonine 11.65mg
+                            Tyrosine 9mg
+                            Tryptophan 2.75mg
+                            Valine 18.7mg
+                            Iron 1.25mg
+                            Magnesium 4.5mg
+                            Cobalt 0.25mcg
+                            Potassium 3.95mg
+                            Zinc 0.6mg`,
+
+            Dosage: `Infants (4–12 months): Half teaspoon TDS.
+                    Children (1–10 years): One teaspoon TID.
+                    Adults: Two teaspoons TDS or 1–2 tablets BD or as prescribed by physician.`,
+
+            Storage: "Store below 35°C.",
+
+            Availability: "Syrup 120ml, Tablets 20's & 30's.",
+
+            Tagline: "Maximum Protein Provider with Lysine & Iron."
         }
-    }
-,
+    },
+
     {
-        id: "abiwin",
-        title: "Abiwin",
-        category: "syrup",
-        img: "./images/syrup-imgs/Abiwin-Syrup.png",
+        id: "lizfol",
+        title: "LIZFOL",
+        category: "tablets",
+        img: "./images/tablet-imgs/Lizfol-tab.png",
         gallery: [
-            "./images/syrup-imgs/Abiwin-Syrup.png"
+            "./images/tablet-imgs/Lizfol-tab.png",
+            "",
+            ""
         ],
-        shortDescription: "Supports immunity and reduces inflammation.",
+        shortDescription: "Affirming you & your baby's safety. L-methylfolate + Vitamin B6 + Vitamin B12 Tablets.",
         details: {
-            Composition: "Serratia Marcescens 10mg",
-            Dosage: "1 teaspoon twice daily.",
-            Indication: "Used for swelling and inflammation.",
-            Storage: "Store below 30°C.",
-            Warning: "Keep out of children's reach."
+            Indications: `LIZFOL is the natural, active form of folic acid used at the cellular level.
+            It bypasses several metabolic steps in the body as it is already present to the absorbable form.
+            LIZFOL is a high absorbable form of folic acid.
+            Contributes to the Normal Maternal Tissue Growth during pregnancy.`,
+
+            ContraIndications: "Consult your doctor or healthcare advisor before use.",
+
+            Composition: `Each Tablet Contains:
+            L-Methyl folate (USP).......400mg
+            Vitamin B6 (USP)...............1.3mg
+            Vitamin B12 (usp) ...............1mcg`,
+
+            Dosage: "One to two tablets daily or as directed by the healthcare advisor.",
+
+            Storage: "Store in a cool and dry place.",
+
+            Availability: "Tablets 20's.",
+
+            Tagline: "Affirming you & your baby's safety."
         }
     }
-,
-    {
-        id: "abiwin",
-        title: "Abiwin",
-        category: "syrup",
-        img: "./images/syrup-imgs/Abiwin-Syrup.png",
-        gallery: [
-            "./images/syrup-imgs/Abiwin-Syrup.png",
-            "./images/syrup-imgs/Abiwin-2.png",
-            "./images/syrup-imgs/Abiwin-3.png"
-        ],
-        shortDescription: "Supports immunity and reduces inflammation.",
-        details: {
-            Composition: "Serratia Marcescens 10mg",
-            Dosage: "1 teaspoon twice daily.",
-            Indication: "Used for swelling and inflammation.",
-            Storage: "Store below 30°C.",
-            Warning: "Keep out of children's reach."
-        }
-    }
-,
-    {
-        id: "abiwin",
-        title: "Abiwin",
-        category: "syrup",
-        img: "./images/syrup-imgs/Abiwin-Syrup.png",
-        gallery: [
-            "./images/syrup-imgs/Abiwin-Syrup.png",
-            "./images/syrup-imgs/Abiwin-2.png",
-            "./images/syrup-imgs/Abiwin-3.png"
-        ],
-        shortDescription: "Supports immunity and reduces inflammation.",
-        details: {
-            Composition: "Serratia Marcescens 10mg",
-            Dosage: "1 teaspoon twice daily.",
-            Indication: "Used for swelling and inflammation.",
-            Storage: "Store below 30°C.",
-            Warning: "Keep out of children's reach."
-        }
-    }
+
 ];
